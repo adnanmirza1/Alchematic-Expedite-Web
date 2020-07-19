b6513efa7bdf85630d47c2ed382189676654de58
adnanmirza1 <mirzaadnanmustafa@yahoo.com>
2020-07-19 13:54:38 +0500

    project added

---
+++ Alchematic/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Alchematic.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Alchematic.Areas.Identity.IdentityHostingStartup))]
+namespace Alchematic.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AlchematicContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AlchematicContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AlchematicContext>();
+            });
+        }
+    }
+}
